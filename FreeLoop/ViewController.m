8080dc36088e711f35a0d81ade11c6b22434f052
naaaaaaq <naaaaaaq@163.com>
2019-01-08 19:03:18 +0800

    freeloop init

--- FreeLoop/ViewController.m
+++ FreeLoop/ViewController.m
@@ -7,6 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <libavformat/avformat.h>
+#import <libavcodec/avcodec.h>
+#import <libswscale/swscale.h>
+#import <libavutil/avutil.h>
+#import <libswresample/swresample.h>
+//#import <libavdevice/avdevice.h>
+#import <libavfilter/avfilter.h>
+
 
 @interface ViewController ()
 
@@ -17,6 +25,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSArray *doc = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *file = [[doc objectAtIndex:0] stringByAppendingPathComponent:@"4.mp4"];
+    
+    av_register_all();
+    avcodec_register_all();
+    avformat_network_init();
+    AVFormatContext *avFormatContext = avformat_alloc_context();
+    
+    NSString *url = @"rtmp://live.hkstv.hk.lxdns.com/live/hks";
+    if (avformat_open_input(&avFormatContext, [file UTF8String], NULL, NULL) != 0) {
+        av_log(NULL, AV_LOG_ERROR, "Couldn't open file");
+    }
+    
+    if (avformat_find_stream_info(avFormatContext, NULL) < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Couldn't find stream information");
+    } else {
+        av_dump_format(avFormatContext, 0, [url cStringUsingEncoding:NSUTF8StringEncoding], NO);
+    }
 }
 
 
